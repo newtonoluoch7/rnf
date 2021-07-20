3d83c784ca75508a9365252fb184b6cff4a0d9c6
newtonoluoch7 <newtonoluoch7@gmail.com>
2021-07-20 10:06:27 +0300

    heroku 1

---
+++ inc/header.php
@@ -0,0 +1,224 @@
+<!doctype html>
+<!--[if IE]><![endif]-->
+<!--[if lt IE 7 ]> <html lang="en" class="ie6">    <![endif]-->
+<!--[if IE 7 ]>    <html lang="en" class="ie7">    <![endif]-->
+<!--[if IE 8 ]>    <html lang="en" class="ie8">    <![endif]-->
+<!--[if IE 9 ]>    <html lang="en" class="ie9">    <![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!-->
+<html class="no-js" lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>RNF|| Home Center Menu</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- Favicon
+		============================================ -->
+		<link rel="shortcut icon" type="image/x-icon" href="img/favicon-32x32.png">
+
+        <!-- master CSS
+        ============================================ -->            
+        <link rel="stylesheet" href="master.css">
+		
+        <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+
+        <!-- Color Css Files Start -->
+        <link rel="alternate stylesheet" type="text/css" href="switcher/color-one.css" title="color-one" media="screen" />
+        <link rel="alternate stylesheet" type="text/css" href="switcher/color-two.css" title="color-two" media="screen" />
+        <link rel="alternate stylesheet" type="text/css" href="switcher/color-three.css" title="color-three" media="screen" />
+        <link rel="alternate stylesheet" type="text/css" href="switcher/color-four.css" title="color-four" media="screen" />
+        <link rel="alternate stylesheet" type="text/css" href="switcher/color-five.css" title="color-five" media="screen" />
+        <link rel="alternate stylesheet" type="text/css" href="switcher/color-six.css" title="color-six" media="screen" />
+        <link rel="alternate stylesheet" type="text/css" href="switcher/color-seven.css" title="color-seven" media="screen" />
+        <link rel="alternate stylesheet" type="text/css" href="switcher/color-eight.css" title="color-eight" media="screen" />
+        <link rel="alternate stylesheet" type="text/css" href="switcher/color-nine.css" title="color-nine" media="screen" />
+        <link rel="alternate stylesheet" type="text/css" href="switcher/color-ten.css" title="color-ten" media="screen" />
+        <link rel="alternate stylesheet" type="text/css" href="switcher/color-eleven.css" title="color-eleven" media="screen" />
+        <link rel="alternate stylesheet" type="text/css" href="switcher/color-twelv.css" title="color-twelve" media="screen" />
+        <link rel="alternate stylesheet" type="text/css" href="switcher/pattren1.css" title="pattren1" media="screen" />
+        <link rel="alternate stylesheet" type="text/css" href="switcher/pattren2.css" title="pattren2" media="screen" />
+        <link rel="alternate stylesheet" type="text/css" href="switcher/pattren3.css" title="pattren3" media="screen" />
+        <link rel="alternate stylesheet" type="text/css" href="switcher/pattren4.css" title="pattren4" media="screen" />
+        <link rel="alternate stylesheet" type="text/css" href="switcher/pattren5.css" title="pattren5" media="screen" />
+        <link rel="alternate stylesheet" type="text/css" href="switcher/background1.css" title="background1" media="screen" />
+        <link rel="alternate stylesheet" type="text/css" href="switcher/background2.css" title="background2" media="screen" />
+        <link rel="alternate stylesheet" type="text/css" href="switcher/background3.css" title="background3" media="screen" />
+        <link rel="alternate stylesheet" type="text/css" href="switcher/background4.css" title="background4" media="screen" />
+        <link rel="alternate stylesheet" type="text/css" href="switcher/background5.css" title="background5" media="screen" />
+        <!-- Color Css Files End -->
+
+    </head>
+    <body>
+        <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+		
+		<!-- Start main area -->
+        <div class="main-area">
+        	<!-- Start header -->
+        	<header class="center-menu">
+        		<!-- Start header top -->
+        		<div class="header-top">
+        			<div class="container">
+        				<div class="row">
+        					<div class="col-md-6">
+	                            <div class="top-social">
+	                                <nav>
+	                                	<ul>
+		                                    <li><a href="#">Follow Us On:</a></li>
+		                                    <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+		                                    <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+		                                    <li><a href="#"><i class="fa fa-google-plus" aria-hidden="true"></i></a></li>
+		                                    <li><a href="#"><i class="fa fa-dribbble" aria-hidden="true"></i></a></li>
+		                                    <li><a href="#"><i class="fa fa-rss" aria-hidden="true"></i></a></li>
+		                                    <li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+		                                </ul>
+	                                </nav>
+	                            </div>
+	                        </div>
+	                        <div class="col-md-6">
+	                            <div class="call-to-action">
+	                                <nav>
+	                                	<ul>
+		                                	<li><a href="mailto:demo@example.com"><i class="fa fa-envelope" aria-hidden="true"></i> Email: info@rn-f.org</a></li>
+		                                	<li><a href="tel:0123456789"><i class="fa fa-phone" aria-hidden="true"></i> Phone: +254 798 484921</a></li>
+		                                	<li><a href="">EN <i class="fa fa-angle-down" aria-hidden="true"></i></a>
+												<ul>
+													<li><a href="#">ENGLISH</a></li>
+													<li><a href="#">FRANCE</a></li>
+													<li><a href="#"></a></li>
+												</ul>
+		                                	</li>
+		                                </ul>
+	                                </nav>
+	                            </div>
+	                        </div>
+        				</div>
+        			</div>
+        		</div>
+        		<!-- End header top -->
+        		<!-- Start main menu area -->
+        		<div class="main-menu-area" id="sticker">
+        			<div class="container">
+        				<div class="row">
+        					<div class="col-sm-12">
+        						<div class="logo ptb-32 text-center">
+        							<a href="index.html">
+        								<img style="width: 200px;" src="img/3x/3x/RNF .svg" alt="">
+        							</a>
+        						</div>
+        					</div>
+        					<div class="col-sm-12">
+                                <div class="main-menu">
+                                    <nav>
+                                        <ul class="d-flex">
+                                            <li><a href="index.html">Home</a></li>
+                                            <li><a href="about.html">About</a></li>
+                                            <li><a href="#">Programmes</a>
+                                                <ul class="drop-menu">
+                                                    <li><a href="scholarships.html">Scholarship</a></li>
+                                                    <li><a href="talentnaturing.html">Talent Naturing</a></li>
+                                                    <li><a href="talent-fairs.html">Talent Fairs</a></li>
+                                                    <li><a href="fellowships.html">RNF Fellowships</a></li>
+                                                    <li><a href="exchange.html">Caltural exchange programs</a></li>
+                                                    <li><a href="community.html">Community Service</a></li>
+                                                    <li><a href="women_youths.html">Women, Girls & Youths</a></li>
+                                                </ul>
+                                            <li><a href="#">Gallery</a>
+                                            <ul class="drop-menu">
+                                                <li><a href="events.html">Events</a></li>
+                                                <li><a href="elements-our-gallery.html">Photos Gallery</a></li>
+                                                <li><a href="elements-video-gallery-4col.html">Videos Gallery</a></li>
+                                            </ul>
+
+                                            </li>
+
+
+                                            <li><a href="blog.html">Blog</a></li>
+                                            <!-- page -->
+                                            <li><a href="#">Participate</a>
+                                                <ul class="drop-menu">
+                                                    <li><a href="contact-us.html">Get Involved</a></li>
+                                                    <li><a href="elements-our-volunteers.html">Be a volunteer</a></li>
+                                                    <li><a href="contact-us.html">Join Us</a></li>
+                                                    <li><a href=".top-social">Tell a Friend</a></li>
+                                                    <li><a href="donate.html">Donate</a></li>
+                                                </ul>
+                                            </li>
+                                            <li><a href="contact-us.html">Contact</a></li>
+                                           
+
+                                            
+                                            <!-- DONATE mordified -->
+                                             <div class="donate-button pl-5">
+                                                <a class="waves-effect waves-light" href="donate.html">Donate</a>
+                                            </div>
+                                        </ul>
+                                        
+                                    </nav>
+                                    
+                                </div>
+                                
+                            </div>
+        				</div>
+        			</div>
+        		</div>
+        		<!-- End main menu area -->
+                <!-- Start mobile menu -->
+                <div class="mobile-menu-area">
+                    <div class="container">
+                        <div class="row">
+                            <div class="mobile-menu">
+                                <nav id="dropdown">
+                                    <ul class="nav">
+                                        <li><a href="index.html">Home</a></li>
+                                        <li><a href="about.html">About</a></li>
+                                        <li><a href="#">Programmes</a>
+                                            <ul>
+                                                <li><a href="scholarships.html">Scholarship</a></li>
+                                                <li><a href="talentnaturing.html">Talent Naturing</a></li>
+                                                <li><a href="talent-fairs.html">Talent Fairs</a></li>
+                                                <li><a href="fellowships.html">RNF Fellowships</a></li>
+                                                <li><a href="exchange.html">Caltural exchange programs</a></li>
+                                                <li><a href="community.html">Community Service</a></li>
+                                                <li><a href="women_youths.html">Women, Girls & Youths</a></li>
+                                            </ul>
+                                        </li>
+                                        <li><a href="#">Gallery</a>
+                                            <ul>
+                                                <li><a href="events.html">Events</a></li>
+                                                <li><a href="elements-our-gallery.html">Photo Gallery</a></li>
+                                                <li><a href="elements-video-gallery-4col.html">Videos Gallery</a></li>
+                                            </ul>
+
+                                            </li>
+                                        
+
+
+                                        <li><a href="blog.html">Blog</a></li>
+                                        <li><a href="#">Participate</a>
+                                            <ul>
+                                                <li><a href="contact-us.html">Get Involved</a></li>
+                                                <li><a href="elements-our-volunteers.html">Be a volunteer</a></li>
+                                                <li><a href="contact-us.html">Join Us</a></li>
+                                                <li><a href=".top-social">Tell a Friend</a></li>
+                                                <li><a href="donate.html">Donate</a></li>
+                                            </ul>
+                                        </li>
+                                        <li><a href="contact-us.html">Contact</a></li>
+
+                                        <!-- Donate -->
+
+                                        <div class="donate-button pl-5">
+                                            <a class="waves-effect waves-light" href="donate.html">Donate</a>
+                                        </div>
+                                        <!-- Donate end -->
+                                    </ul>
+                                </nav>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- End mobile menu -->
+        	</header>
